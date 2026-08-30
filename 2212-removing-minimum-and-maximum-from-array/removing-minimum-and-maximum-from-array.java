30ec2ed086cb370b2cd5e4badfd896a6f8c6f8e0
ivivekkushwaha <135034373+ivivekkushwah@users.noreply.github.com>
2026-08-30 12:25:06 +0530

    Time: 2 ms (100.00%) | Memory: 86.9 MB (23.09%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
@@ -1,37 +1,18 @@
 class Solution {
     public int minimumDeletions(int[] nums) {
-        // HashMap<Integer, Integer> hm = new HashMap<>();
-        int maxI =0, minI=0, maxV = Integer.MIN_VALUE, minV = Integer.MAX_VALUE;
-        int n= nums.length;
-        for(int i=0; i<n; i++){
-            if(nums[i]>maxV){
-                maxV = nums[i];
-                maxI = i;
-            }
-            if(nums[i]<minV){
-                minV = nums[i];
-                minI = i;
-            }
+        int n = nums.length;
+        int minI = 0, maxI = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[minI]) minI = i;
+            if (nums[i] > nums[maxI]) maxI = i;
         }
-        int ahead = Math.max(maxI, minI);
-        int before = Math.min(maxI, minI);
-        int total = 0;
-            if(before+1<=n-ahead){
-                total += before+1;
-                if(ahead-before<=n-ahead){
-                    total += ahead-before;
-                }else{
-                    total += n-ahead;
-                }
-            }else{
-                total += n-ahead;
-                if(ahead-before<=before+1){
-                    total += ahead-before;
-                }else{
-                    total += before+1;
-                }
-            }
-        return total;
-         
+
+        int left = Math.max(minI, maxI) + 1;
+        int right = n - Math.min(minI, maxI);
+        int both = Math.min(minI, maxI) + 1
+                 + n - Math.max(minI, maxI);
+
+        return Math.min(left, Math.min(right, both));
     }
 }

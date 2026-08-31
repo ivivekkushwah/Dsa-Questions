555420793cb907204f7cca03829c46f115fc30ef
ivivekkushwaha <135034373+ivivekkushwah@users.noreply.github.com>
2026-08-31 21:31:09 +0530

    Time: 4 ms (99.87%) | Memory: 106 MB (85.47%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -1,76 +1,38 @@
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        int[] arr = {-1, -1};
+        int[] ans = {-1, -1};
 
-        int i = 2;
-        int minD = Integer.MAX_VALUE;
-        int maxD = 0;
-        int firstC = 0;
-        int prevC = 0;
+        ListNode prev = head;
+        ListNode curr = head.next;
 
-        return checkMiniandMaxi(
-            head,
-            head.next,
-            head.next.next,
-            i,
-            arr,
-            minD,
-            maxD,
-            firstC,
-            prevC
-        );
-    }
+        int index = 1;
+        int first = -1;
+        int last = -1;
+        int min = Integer.MAX_VALUE;
 
-    public int[] checkMiniandMaxi(
-        ListNode prev,
-        ListNode curr,
-        ListNode after,
-        int i,
-        int[] arr,
-        int minD,
-        int maxD,
-        int firstC,
-        int prevC
-    ) {
-        if (after == null) {
-            if (firstC != 0 && prevC != firstC) {
-                arr[0] = minD;
-                arr[1] = maxD;
+        while (curr.next != null) {
+            if ((curr.val > prev.val && curr.val > curr.next.val) ||
+                (curr.val < prev.val && curr.val < curr.next.val)) {
+
+                if (first == -1) {
+                    first = index;
+                } else {
+                    min = Math.min(min, index - last);
+                }
+
+                last = index;
             }
-            return arr;
+
+            prev = curr;
+            curr = curr.next;
+            index++;
         }
 
-        boolean critical =
-            (prev.val < curr.val && curr.val > after.val) ||
-            (prev.val > curr.val && curr.val < after.val);
-
-        if (critical) {
-
-            if (prevC != 0) {
-                minD = Math.min(minD, i - prevC);
-            }
-
-            if (firstC != 0) {
-                maxD = Math.max(maxD, i - firstC);
-            }
-
-            if (firstC == 0) {
-                firstC = i;
-            }
-
-            prevC = i;
+        if (first != -1 && first != last) {
+            ans[0] = min;
+            ans[1] = last - first;
         }
 
-        return checkMiniandMaxi(
-            curr,
-            after,
-            after.next,
-            i + 1,
-            arr,
-            minD,
-            maxD,
-            firstC,
-            prevC
-        );
+        return ans;
     }
 }

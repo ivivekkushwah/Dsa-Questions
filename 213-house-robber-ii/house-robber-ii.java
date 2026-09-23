d772cca0058d304a34135d8b625b3b55f1937e7a
ivivekkushwaha <135034373+ivivekkushwah@users.noreply.github.com>
2026-09-23 18:35:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (8.60%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -5,33 +5,22 @@ class Solution {
         if (nums.length == 1)
             return nums[0];
 
-        int[] dp1 = new int[nums.length + 1];
-        int[] dp2 = new int[nums.length + 1];
-
-        Arrays.fill(dp1, -1);
-        Arrays.fill(dp2, -1);
-
         // Exclude first house: 1 -> n-1
-        int l = fun(dp1, 1, nums, nums.length - 1);
+        int l = fun( 1, nums, nums.length - 1);
 
         // Exclude last house: 0 -> n-2
-        int r = fun(dp2, 0, nums, nums.length - 2);
+        int r = fun( 0, nums, nums.length - 2);
 
         return Math.max(l, r);
     }
-
-    public int fun(int[] dp, int idx, int[] nums, int end) {
-
-        if (idx > end)
-            return 0;
-
-        if (dp[idx] != -1)
-            return dp[idx];
-
-        int loot = nums[idx] + fun(dp, idx + 2, nums, end);
-
-        int noLoot = fun(dp, idx + 1, nums, end);
-
-        return dp[idx] = Math.max(loot, noLoot);
+    public int fun(int start, int nums[], int end){
+        int[] dp = new int[end + 3];
+        
+        for(int i=end; i>=start; i--){
+            int include = nums[i] + dp[i+2];
+            int exclude = dp[i+1];
+            dp[i] = Math.max(include, exclude);
+        }
+        return dp[start];
     }
 }
